07f1efe060e027a390db843d86a3884f2ff88670
Christopher Warren <warre213@warre213.asr.umn.edu>
2009-03-23 13:34:15 -0500

    * Split plugins out to their own file

--- base.rb
+++ base.rb
@@ -26,12 +26,7 @@ rake("gems:install")
 rake("rails:freeze:gems") if yes?("Freeze Rails gems?")
 
 # Set up plugins
-plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git'
-plugin 'authlogic', :git => 'git://github.com/binarylogic/authlogic.git'
-plugin 'paperclip', :git => 'git://github.com/thoughtbot/paperclip.git'
-plugin 'asset_packager', :git => 'http://synthesis.sbecker.net/pages/asset_packager'
-plugin 'lazy_developer', :git => 'git://github.com/napcs/lazy_developer.git'
-plugin 'annotate_models', :svn => 'http://repo.pragprog.com/svn/Public/plugins/annotate_models'
+load_template "#{@base_path}/plugins.rb"
 
 # Commit to git if we created the repo
 git :add => "." if @using_git
---
+++ plugins.rb
@@ -0,0 +1,6 @@
+plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git'
+plugin 'authlogic', :git => 'git://github.com/binarylogic/authlogic.git'
+plugin 'paperclip', :git => 'git://github.com/thoughtbot/paperclip.git'
+plugin 'asset_packager', :git => 'http://synthesis.sbecker.net/pages/asset_packager'
+plugin 'lazy_developer', :git => 'git://github.com/napcs/lazy_developer.git'
+plugin 'annotate_models', :svn => 'http://repo.pragprog.com/svn/Public/plugins/annotate_models'
